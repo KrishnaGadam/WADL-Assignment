8a8e3612034b61f308d740276b75eba12be61ed6
KrishnaGadam <79629777+KrishnaGadam@users.noreply.github.com>
2022-02-10 12:13:07 +0530

    Add files via upload
    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//  const signIn=e=>{
+//     let data={
+//       email:document.getElementById("floatingInput").value,
+//       password:document.getElementById("floatingPassword").value
+//     }
+//     localStorage.setItem("formData",JSON.stringify(data));
+//     console.log(localStorage.getItem("formData"));
+//     //e.preventDefault(); 
+//     alert();   
+// }
+
+
+
+$(document).ready(function () {
+  $("#Submit").click(function () {
+     $.post("/request",
+        {
+          email:document.getElementById("floatingInput").value,
+          password: document.getElementById("floatingPassword").value
+        },
+        function (data, status) {
+           console.log(status);
+        });
+    //alert(document.getElementById("floatingInput").value);
+    
+  });
+});
